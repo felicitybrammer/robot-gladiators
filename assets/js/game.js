644a9a7a7e880393fdcd3f1c04257d49fe4ff6d0
Felicity Brammer <felicitybrammer@gmail.com>
2021-08-30 11:20:13 -0700

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,8 +74,20 @@ var fight = function(enemyName) {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        //let player know what round they are in 
+        window.alert("Welcome to Robot Gladiators! Round " + (i +1));
+        //pick new enemy to fight
+        var pickedEnemyName = enemyNames[i];
+        //reset enemyHealth before starting new fight
+        enemyHealth = 50;
+        //pass the pickedEnemyName variable's value into the fight function
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+    }
+
+
+    
 }
 
