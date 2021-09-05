692a961329a249e0dbc5dac94632f815ec043664
Felicity  Brammer <felicitybrammer@MacBook-Air-7.hitronhub.home>
2021-09-05 12:54:32 -0700

    save high score using localStorage

--- assets/js/game.js
+++ assets/js/game.js
@@ -115,12 +115,29 @@ var startGame = function() {
 
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
-    } else {
-        window.alert("You've lost your robot in battle.");
+    
+    //check localStorage for high score, if not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
     }
+
+    //if player has more money than the high score, player has a new high score
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highScore", playerInfo.money);
+        localStorage.getItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    } else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ".");
+    }
+
+
+    // if (playerInfo.health > 0) {
+    //     window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    // } else {
+    //     window.alert("You've lost your robot in battle.");
+    // }
 
     //ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
@@ -147,7 +164,7 @@ var shop = function() {
         case 2:
             playerInfo.upgradeAttack();
             break;
-        case 2:
+        case 3:
             window.alert("Leaving the store.");
             break;
         default:
